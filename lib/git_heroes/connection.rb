2dc2d5cb6110aeea7518c9229885dae76e7f83ea
Julien Letessier <julien.letessier@gmail.com>
2016-02-08 15:50:53 +0100

    Fixes caching

--- lib/git_heroes/connection.rb
+++ lib/git_heroes/connection.rb
@@ -6,17 +6,22 @@ require 'git_heroes/logger'
 require 'git_heroes/serializer'
 require 'git_heroes/ext/faraday'
 
+require 'redis/store'
+require 'active_support'
+
 class GitHeroes::Connection < SimpleDelegator
   def initialize(token:nil)
     stack = Faraday::Builder.new do |builder|
       builder.use(:http_cache,
-        store:         :redis_store,
-        store_options: %w(localhost/0/githeroes),
+        store:         ActiveSupport::Cache.lookup_store(:redis_store, %w(localhost/0/githeroes)),
+        # store:         :redis_store,
+        # store_options: %w(localhost/0/githeroes),
         serializer:    GitHeroes::Serializer,
         logger:        GitHeroes::Logger.instance)
       builder.use     GitHeroes::Cache
       builder.use     Octokit::Response::RaiseError
-      builder.adapter Faraday.default_adapter
+      # builder.adapter Faraday.default_adapter
+      builder.adapter :net_http_persistent
     end
 
     client = Octokit::Client.new(
